ddb5276956391f4982b6b20a23073133056ca837
Juno Student <j.morey@berkeley.edu>
2015-09-25 23:38:38 -0700

    q3 not working

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,10 @@
 class Foobar
-  # ENTER CODE FOR Q2 HERE
+
+  def initialize(s)
+  	@s = s
+  end
+
+  def bar(sym, dict)
+  	return sym.to_s + @s + dict.values[0].to_s
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,15 @@
 class PagesController < ApplicationController
   def home
-    # foo = Foobar.new "baz"
-    # @baz = foo.bar :cat, sat: :dat, dat: :sat
+    foo = Foobar.new "baz"
+    @baz = foo.bar :cat, sat: :dat, dat: :sat
   end
 
   def stringify
+    if not params[:name].blank? 
+      @text = params[:name] + " is " + params[:adjective] + "!"
+    else
     @text = "You are nothing!"
+    end
   end
 
   def age
